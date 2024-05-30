3be073e31aba07e3b349d2f55766db88ad5e20e8
Sona <136668031+Sona2610@users.noreply.github.com>
2024-05-30 10:26:55 +0530

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,30 @@
+import { useReducer } from "react"
+import Student from "./components/Student"
+import ChildComponent from "./components/ChildComponent"
+import Arraysample from "./components/Arraysample";
+import Oneofsample from "./components/Oneofsample";
+
+function App() {
+// const items=[
+// {id:1 ,name:"Item1"},
+// {id:2 ,name:"Item2"},
+// {id:3 ,name:"Item3"},
+// ];
+return (
+<>
+    {/* { <Student name="Sona" age={21} isMarried={true}/>
+  <Student name="Sandhiya" age={21} isMarried={false}/>
+  <Student name="Sangeetha" age={26} isMarried={false}/>
+  <ChildComponent>
+    <p>This 1</p>
+    <p>This 2</p>
+  </ChildComponent> }
+  <Oneofsample color="green"/>
+<Arraysample items={items}/> */}
+
+</>
+  
+)
+
+}
+export default App
